d21c56a2854032631090b84fab63753d027375b1
Nikita <neymarenko123@gmail.com>
2020-11-20 23:07:56 +0200

    default config

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './appStyles.scss';
+import {hot} from 'react-hot-loader/root';
+
+const App = () => {
+    return (
+        <main className="app">
+            My App
+        </main>
+    );
+};
+
+export default hot(App);
